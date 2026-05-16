ff621a8d08a98c990467e47736a07a48e6024d4e
ElOsasInc <rosas.axel.b@gmail.com>
2026-05-16 00:48:42 -0600

    GET Dashboard - completo

--- controllers/dashboard_controller.py
+++ controllers/dashboard_controller.py
@@ -9,7 +9,12 @@ class DashboardController:
     def get_dashboard_stats(self) -> List[Dict[str, Any]]:
         try:
             dashboard = {
-                "ventas_30dias": self.model.get_ventas_30dias()
+                "ventas_30dias": self.model.get_ventas_30dias(),
+                "afluencia": self.model.get_afluencia(),
+                "top5_prods_pedidos": self.model.get_top5_prods_pedidos(),
+                "top5_vinos_vendidos": self.model.get_top5_vinos_vendidos(),
+                "pedidos_10semanas": self.model.get_pedidos_10semanas(),
+                "costo_ganancia": self.model.get_costo_ganancia()
             }
             return dashboard
         except Exception as e:
--- models/dashboard_model.py
+++ models/dashboard_model.py
@@ -58,4 +58,86 @@ class DashboardModel:
             """)
             return cur.fetchall()
         
-    
+    def get_afluencia(self) -> List[Dict[str, Any]]:
+        with self.db.cursor() as cur:
+            cur.execute("""
+                WITH horas as (
+                    SELECT GENERATE_SERIES(0, 23, 1)::INT as hora
+                )
+                SELECT h.hora, COALESCE(cant_ventas, 0) as cant_ventas
+                FROM horas as h
+                LEFT JOIN (
+                    SELECT EXTRACT(HOUR FROM fecha_hora) as hora, COUNT(id_venta) as cant_ventas
+                    FROM venta
+                    GROUP BY hora
+                ) as v ON h.hora = v.hora
+                ORDER BY h.hora;
+            """)
+            return cur.fetchall()
+        
+    def get_top5_prods_pedidos(self) -> List[Dict[str, Any]]:
+        with self.db.cursor() as cur:
+            cur.execute("""
+                SELECT v.nombre, SUM(cantidad) as cantidad, ROUND(SUM(costo_unitario*cantidad)::NUMERIC, 2) as dinero_total
+                FROM producto_pedido as pp INNER JOIN vino as v ON v.vino_id = pp.vino_id
+                GROUP BY v.nombre
+                ORDER BY cantidad DESC
+                LIMIT 5;
+            """)
+            return cur.fetchall()
+        
+    def get_top5_vinos_vendidos(self) -> List[Dict[str, Any]]:
+        with self.db.cursor() as cur:
+            cur.execute("""
+                SELECT nombre, SUM(ml) as ml_total, ROUND(SUM(registro_precio)::NUMERIC, 2) as precio_total
+                FROM producto_vendido as pv
+                INNER JOIN botella as b ON b.id_botella = pv.id_botella INNER JOIN vino as v ON v.vino_id = b.vino_id
+                GROUP BY nombre
+                ORDER BY ml_total DESC
+                LIMIT 5;
+            """)
+            return cur.fetchall()
+        
+    def get_pedidos_10semanas(self) -> List[Dict[str, Any]]:
+        with self.db.cursor() as cur:
+            cur.execute("""
+                WITH semanas as (
+                    SELECT GENERATE_SERIES(
+                        DATE_TRUNC('week', (NOW() - INTERVAL '9 weeks'))::DATE,
+                        DATE_TRUNC('week', NOW())::DATE, '1 week'::INTERVAL
+                    )::DATE as semana
+                )
+                SELECT s.semana, COALESCE(total_pedidos, 0) as total_pedidos
+                FROM semanas as s
+                INNER JOIN (
+                    SELECT DATE_TRUNC('week', fecha_pedido)::DATE as semana, COUNT(pedido_id) as total_pedidos
+                    FROM pedido
+                    GROUP BY semana
+                ) as p ON p.semana = s.semana
+                ORDER BY semana;
+            """)
+            return cur.fetchall()
+
+    def get_costo_ganancia(self) -> List[Dict[str, Any]]:
+        with self.db.cursor() as cur:
+            cur.execute("""
+                WITH meses as (
+                    SELECT TO_CHAR(GENERATE_SERIES( 
+                        (NOW() - INTERVAL '11 months')::DATE, NOW()::DATE, '1 month'::INTERVAL
+                    )::DATE, 'YYYY MON') as mes
+                )
+                SELECT m.mes, COALESCE(costo_total, 0.00) as costo_total, COALESCE(ganancia_total, 0.00) as ganancia_total
+                FROM meses as m
+                INNER JOIN (
+                    SELECT TO_CHAR(fecha_pedido, 'YYYY MON') as mes, ROUND(SUM(costo_unitario*cantidad)::NUMERIC, 2) as costo_total, g.ganancia_total
+                    FROM producto_pedido as pp INNER JOIN pedido as p ON p.pedido_id = pp.pedido_id
+                    FULL OUTER JOIN (
+                        SELECT TO_CHAR(fecha_hora, 'YYYY MON') as mes, ROUND(SUM(registro_precio)::NUMERIC, 2) as ganancia_total
+                        FROM producto_vendido as pv INNER JOIN venta as v ON v.id_venta = pv.id_venta
+                        GROUP BY mes
+                    ) as g ON g.mes = TO_CHAR(fecha_pedido, 'YYYY MON')
+                    GROUP BY TO_CHAR(fecha_pedido, 'YYYY MON'), g.ganancia_total
+                ) as cg ON cg.mes = m.mes
+                ORDER BY TO_DATE(m.mes, 'YYYY MON');
+            """)
+            return cur.fetchall()
