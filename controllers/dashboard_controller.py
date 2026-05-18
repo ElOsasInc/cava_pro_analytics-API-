e1898fc773c12e5ea11e812dc3ccfb1ae82f436d
ThinKreations <jnapolesm1800@alumno.ipn.mx>
2026-05-18 11:52:51 -0600

    dashboard_controller - tipo de graficas

--- controllers/dashboard_controller.py
+++ controllers/dashboard_controller.py
@@ -9,12 +9,30 @@ class DashboardController:
     def get_dashboard_stats(self) -> List[Dict[str, Any]]:
         try:
             dashboard = {
-                "ventas_30dias": self.model.get_ventas_30dias(),
-                "afluencia": self.model.get_afluencia(),
-                "top5_prods_pedidos": self.model.get_top5_prods_pedidos(),
-                "top5_vinos_vendidos": self.model.get_top5_vinos_vendidos(),
-                "pedidos_10semanas": self.model.get_pedidos_10semanas(),
-                "costo_ganancia": self.model.get_costo_ganancia()
+                "ventas_30dias":{
+                    "tipo":"Line",
+                    "data": self.model.get_ventas_30dias()
+                },
+                "afluencia":{
+                    "tipo": "Bar",
+                    "data": self.model.get_afluencia()
+                },
+                "top5_prods_pedidos":{
+                    "tipo": "Bar",
+                    "data": self.model.get_top5_prods_pedidos()
+                },
+                "top5_vinos_vendidos":{
+                    "tipo": "Bar",
+                    "data": self.model.get_top5_vinos_vendidos()
+                },
+                "pedidos_10semanas":{
+                    "tipo": "Line",
+                    "data": self.model.get_pedidos_10semanas()
+                },
+                "costo_ganancia":{
+                    "tipo": "Line",
+                    "data": self.model.get_costo_ganancia()
+                }
             }
             return dashboard
         except Exception as e:
